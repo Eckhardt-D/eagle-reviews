ab5bc87d3d04cfa41476437e7f0b5af71d0349cb
ecki-d <support@kaizenmedia.co.za>
2018-12-11 10:45:16 +0200

    Create client-side actions to make secure requests to server with JWT

--- client/store/index.js
+++ client/store/index.js
@@ -29,36 +29,59 @@ const actions = {
     .catch(err => console.log(err))
   },
   authenticateUser({commit, dispatch}) {
-    if(!auth.currentUser) {
-      auth.signInWithRedirect(googleAuth).then(function(result) {
-        var user = result.user;
-        commit('SET_USER', user);
-        this.$router.push('/');
-      }).catch(function(error) {
-        commit('SET_USER', {});
-      });
-    } else {
-      console.log(auth.currentUser)
-      dispatch('saveUserToDb', auth.currentUser)
-    }
-  },
-  saveUserToDb(ctx, user) {
-    return new Promise((resolve, reject) => {
-      let userDetails = {
-        googleId: user.uid,
-        firstname: user.displayName,
-        owns: [''],
-        reviews: [{}],
-        favourites: [{}],
-        email: user.email,
-        reported: false,
-        profileImage: user.photoURL
-      }
+    auth.signInWithRedirect(googleAuth)
+    .then(result => {
+      let user = result.user
 
-      this.$axios.$post('http://localhost:5000/users', userDetails)
-      .then(res => console.log(res))
-      .catch(e => console.log(e))
+      dispatch('getUserToken', user)
+      .then(data => {
+        commit('SET_USER', data.user)
+      })
+      .catch(error => console.log(error))
     })
+    .catch(error => console.log(error))
+  },
+  getUserToken(ctx, user) {
+    return new Promise((resolve, reject) => {
+      let token = JSON.parse(localStorage.getItem('ERTOKEN'));
+
+      if(typeof token === 'undefined' || token === null) {
+        let userDetails = {
+          googleId: user.uid,
+          firstname: user.displayName,
+          owns: [''],
+          reviews: [{}],
+          favourites: [{}],
+          email: user.email,
+          reported: false,
+          profileImage: user.photoURL
+        }
+  
+        this.$axios.$post('http://localhost:5000/users', userDetails)
+        .then(res => {
+          token = res.token
+
+          localStorage.setItem('ERTOKEN', JSON.stringify(token))
+          resolve(res)
+        })
+        .catch(e => reject(e))
+      } else {
+        resolve({user, token})
+      }
+    })
+  },
+  getRequestSecurity({dispatch}, [user, callback]) {
+    let theUser = user ? user : auth.currentUser
+
+    if(theUser) {
+      dispatch('getUserToken', theUser)
+      .then(data => {
+        callback(data.token)
+      })
+      .catch(error => console.log(error));
+    } else {
+      this.$router.go('/test')
+    }
   }
 }
 
